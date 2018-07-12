6f0c5a7c367396f90ded83d98f0dddc6bc0d47cd
il <real88c@gmail.com>
2018-07-12 12:49:13 +0000

    use "read" to give args

--- httpBot.sh
+++ httpBot.sh
@@ -1,5 +1,10 @@
-host='127.0.0.1:8000'
-delay='1' #as sec
+echo 'input host name :'
+host=read
+echo 'input delay as sec :'
+delay=read
+
+#host='127.0.0.1:8000'
+#delay='1' #as sec
 
 while true
 do
